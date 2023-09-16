8ecf673b03ecedefec7230b1a1762782961f5114
Owen <osowatzke@gmail.com>
2023-09-15 19:38:19 -0700

    Finished Homework 2

---
+++ 2/semilog_mag_freq_response_sowatzke.m
@@ -0,0 +1,39 @@
+% Author: O. Sowatzke
+%
+% Updated: 09/19/2023
+%
+% Subject: Script plots the semilog magnitude frequency response of a
+% filter specified by a difference equation
+
+close all;
+clear;
+
+% Numerator and denominator coefficients of transfer function
+% of difference equation specified in problem 5.
+% Type "help filter" for more details on
+a = [1, 1.6511, 1.0893, 0.2447];
+b = [0.0585, -0.0382, 0.0382, -0.0585];
+
+% Save 512 samples of the freq response
+% 'whole' outputs frequency response over interval [0 2*pi)
+[H,w] = freqz(b,a,'whole');
+
+% semilog plot of the mag freq response
+% fftshift allows us to plot frequency response over interval [-pi pi) 
+semilogy(w-pi,fftshift(abs(H)),'lineWidth',1.5);
+
+% add labels to each axis and a title
+xlabel('\omega');
+ylabel('|H(e^{j\omega})|');
+title('Semilogarithmic Magnitude Frequency Response');
+
+% Set Bounds of xaxis
+xlim([-pi pi]);
+
+% Set ticks on x axis and label ticks in terms of pi
+xticks(-pi:pi/4:pi);
+xticklabels({'-\pi','-3\pi/4','-\pi/2','-\pi/4','0',...
+    '\pi/4','\pi/2','3\pi/4','\pi'});
+
+% Turn on grid for readability
+grid on;
