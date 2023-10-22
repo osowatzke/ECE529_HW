2b5387662d4e76533c744217cfe3c964f7640179
Owen <osowatzke@gmail.com>
2023-10-22 15:38:51 -0700

    Started working on final homework problem

--- 4/Homework4_Sowatzke_Plots.m
+++ 4/Homework4_Sowatzke_Plots.m
@@ -57,3 +57,68 @@ fill(x,-y,'k','FaceAlpha',0.1,'LineStyle','none');
 
 % Denote poles and zeros at infinity
 text(0.25,0.75,'X @ z = \infty');
+
+%% Problem 3.40a)
+% Create pole-zero map for Y(z)
+f = figure(2);
+clf;
+hold on;
+
+% Make figure square
+pos = f.Position;
+pos(2) = pos(2) + (pos(4) - pos(3))/2;
+pos(4) = pos(3);
+f.Position = pos;
+
+% plot poles and zeros
+% using scatter instead of pzmap to customize marker size
+scatter([0.5 2], [0 0], 72, 'kx', 'lineWidth',0.75);
+scatter([0 0.25], [0 0], 72, 'ko', 'lineWidth',0.75);
+
+% plot real and imaginary axis
+plot([-1 2],[0 0],':k');
+plot([0 0],[-1.5 1.5],':k');
+
+% plot unit circle
+x = -1:0.001:1;
+y = sqrt(1-x.^2);
+plot(x,y,':k');
+plot(x,-y,':k');
+
+% Label plot
+title('Pole-Zero Map')
+xlabel('Real Axis');
+ylabel('Imaginary Axis');
+box on;
+
+% Create pole-zero map for X(z)
+f = figure(3);
+clf;
+hold on;
+
+% Make figure square
+pos = f.Position;
+pos(2) = pos(2) + (pos(4) - pos(3))/2;
+pos(4) = pos(3);
+f.Position = pos;
+
+% plot poles and zeros
+% using scatter instead of pzmap to customize marker size
+scatter([-0.75 0.5], [0 0], 72, 'kx', 'lineWidth',0.75);
+scatter(0.25, 0, 72, 'ko', 'lineWidth',0.75);
+
+% plot real and imaginary axis
+plot([-1 1],[0 0],':k');
+plot([0 0],[-1 1],':k');
+
+% plot unit circle
+x = -1:0.001:1;
+y = sqrt(1-x.^2);
+plot(x,y,':k');
+plot(x,-y,':k');
+
+% Label plot
+title('Pole-Zero Map')
+xlabel('Real Axis');
+ylabel('Imaginary Axis');
+box on;
